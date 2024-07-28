f2f81bd88f6e70fc551fceb5cc4045c33f12aa28
Alamin573190 <mdalaminhossain573190@gmail.com>
2024-07-29 00:29:32 +0600

    added resume download link

--- src/Componants/Banner.jsx
+++ src/Componants/Banner.jsx
@@ -93,7 +93,7 @@ const Banner = () => {
             <button className="btn btn-outline btn-error">
               <Link
                 className="text-black font-bold"
-                to="https://drive.google.com/u/0/uc?id=1-FD6cv1x5sM6JhtEss0HqOYEL1whs5vj&export=download"
+                to="https://drive.google.com/u/0/uc?id=15n24ONvLTMkgMsMHkwqvaVWSQf8bkyTA&export=download"
               >
                 Download resume
               </Link>
